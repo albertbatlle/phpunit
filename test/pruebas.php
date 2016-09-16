1aaf5aeb0d3db8795cd2946d8f849ca61f6cdadc
albertbatlle <albert.batlle@gmail.com>
2016-09-16 14:10:17 +0200

    VM2

---
+++ test/pruebas.php
@@ -0,0 +1,41 @@
+<?php
+
+  /****************** ARRAY MAYOR Y MENOR *****************/
+  $cantidad=array(3,4,6,1,65,-3,6,10);
+  $i=0;
+  $menor=$cantidad[$i];
+  $mayor=$cantidad[$i];
+  while($i<=7)
+  {
+    if($menor>$cantidad[$i])
+    {
+      $menor=$cantidad[$i];
+    }
+    $i=$i+1;
+  }
+  echo "$menor <br>";
+  $cantidad=array(3,4,6,1,65,-3,6,10);
+  $i=0;
+  $mayor=$cantidad[$i];
+  while($i<=7)
+  {
+    if($mayor<$cantidad[$i])
+    {
+      $mayor=$cantidad[$i];
+    }
+    $i=$i+1;
+  }
+  echo "$mayor <br>";
+  $cantidad=array(3,4,6,1,65,-3,6,10);
+  $menor=$menor;
+  $i=0;
+  while ($i<4){
+    if($menor>=$cantidad[$i]){
+      $menor=$cantidad[$i];
+    }
+    echo "menor <br>";
+  }
+/********************* REPETICION ARRAY ************************/
+  $array = array (1,5,78,54,5,5,78,2,5,5);
+  print_r(array_count_values($array));
+ ?>
